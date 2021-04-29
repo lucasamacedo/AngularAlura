a6ece01599648fc059d114825856628624c77c55
lucasamacedo <lucas_am_@hotmail.com>
2021-04-29 12:29:51 -0300

    Conseguindo exibir a página

--- C1_bytebank/src/app/nova-tranferencia/nova-transferencia.component.ts
+++ C1_bytebank/src/app/nova-tranferencia/nova-transferencia.component.ts
@@ -1,9 +1,9 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector:'nova-transferencia',
-    templateUrl:'./nova-transferencia.component.html',
-    styleUrls:['./nova-transferencia.component.scss']
+    selector: 'app-nova-transferencia',
+    templateUrl: './nova-transferencia.component.html',
+    styleUrls: ['./nova-transferencia.component.scss']
 })
 export class NovaTransferenciaComponent{
 
