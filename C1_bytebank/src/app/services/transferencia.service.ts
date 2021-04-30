de70e958091eede5873110a9f192c227dd671641
lucasamacedo <lucas_am_@hotmail.com>
2021-04-30 11:05:45 -0300

    Integrando os extratos com a API

--- C1_bytebank/src/app/services/transferencia.service.ts
+++ C1_bytebank/src/app/services/transferencia.service.ts
@@ -1,26 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Transferencia } from '../model/transferencia.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransferenciaService {
   private listaTransferencia: any[];
+  private url = 'http://localhost:3000/transferencias';
 
-  constructor() {
+  constructor(private httpClient: HttpClient) {
     this.listaTransferencia = [];
   }
 
-  get transferencias(){
+  get transferencias(): any{
     return this.listaTransferencia;
   }
 
-  adicionar(transferencia: any){
+  todas(): Observable<Transferencia[]>{
+    return this.httpClient.get<Transferencia[]>(this.url);
+  }
+
+  adicionar(transferencia: any): void{
     this.hidratar(transferencia);
 
     this.listaTransferencia.push(transferencia);
   }
 
-  private hidratar(transferencia: any){
+  private hidratar(transferencia: any): void{
     transferencia.data = new Date();
   }
 }
