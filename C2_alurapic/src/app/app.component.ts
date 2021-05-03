14e9598f1c75e51ffa54bcb3419df844cb2aec78
lucasamacedo <lucas_am_@hotmail.com>
2021-05-03 09:35:03 -0300

    Criando componente Photo

--- C2_alurapic/src/app/app.component.ts
+++ C2_alurapic/src/app/app.component.ts
@@ -6,7 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'alurapic';
-  description = 'Máscara';
-  url = 'https://i0.wp.com/dozebits.com.br/wp-content/uploads/2016/09/dishonored_2_corvo_mask.jpg?fit=650%2C450&ssl=1';
+
 }
---
+++ C2_alurapic/src/app/photo/photo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photo',
+  templateUrl: 'photo.component.html'
+})
+export class PhotoComponent{
+  description = 'Máscara';
+
+  url = 'https://i0.wp.com/dozebits.com.br/wp-content/uploads/2016/09/dishonored_2_corvo_mask.jpg?fit=650%2C450&ssl=1';
+}
