77e0af08baa8e4c0bf8881e877dc0bf7d7093328
lucasamacedo <lucas_am_@hotmail.com>
2021-05-03 08:51:29 -0300

    Criando aplicacao Angular e adicionando Bootstrap

---
+++ C2_alurapic/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'alurapic';
+  description = 'Máscara';
+  url = 'https://i0.wp.com/dozebits.com.br/wp-content/uploads/2016/09/dishonored_2_corvo_mask.jpg?fit=650%2C450&ssl=1';
+}
