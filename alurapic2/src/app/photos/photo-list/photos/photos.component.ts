6f00c8a188421d7cd3f9c74418126faecc664b9c
lucasamacedo <lucas_am_@hotmail.com>
2021-05-04 14:53:31 -0300

    Implementando componente de separação da lista em colunas e linhas

--- alurapic2/src/app/photos/photo-list/photos/photos.component.ts
+++ alurapic2/src/app/photos/photo-list/photos/photos.component.ts
@@ -9,10 +9,20 @@ import { Photo } from '../../photo/photo';
 export class PhotosComponent implements OnInit {
 
   @Input() photos: Photo[] = [];
+  rows: any[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.rows = this.groupColumns(this.photos);
+  }
+
+  groupColumns(photos: Photo[]): any[]{
+    const newRows: any[] = [];
+    for(let i = 0; i < photos.length; i += 3){
+      newRows.push(photos.slice(i, i + 3));
+    }
+    return newRows;
   }
 
 }
