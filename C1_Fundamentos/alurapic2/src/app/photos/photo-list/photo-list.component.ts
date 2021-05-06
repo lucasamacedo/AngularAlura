32229737f1d14e1548d9fe457ea508041cda735a
lucasamacedo <lucas_am_@hotmail.com>
2021-05-06 10:22:06 -0300

    Organizando pastas e adicionando readme.txt

---
+++ C1_Fundamentos/alurapic2/src/app/photos/photo-list/photo-list.component.ts
@@ -0,0 +1,40 @@
+import { PhotoService } from './../photo/photo.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Photo } from '../photo/photo';
+
+@Component({
+  selector: 'app-photo-list',
+  templateUrl: './photo-list.component.html',
+  styleUrls: ['./photo-list.component.scss']
+})
+export class PhotoListComponent implements OnInit {
+
+  photos: Photo[] = [];
+  filter = '';
+  hasMore = true;
+  currentPage = 1;
+  userName = '';
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
+    ){ }
+
+  ngOnInit(): void{
+    this.userName = this.activatedRoute.snapshot.params.userName;
+    this.photos = this.activatedRoute.snapshot.data.photos;
+  }
+
+  load(): void {
+    this.photoService
+    .listFromUserPaginated(this.userName, ++this.currentPage)
+    .subscribe(photos => {
+      this.filter = '';
+      this.photos = this.photos.concat(photos);
+      if(!photos.length) { this.hasMore = false; }
+    });
+
+  }
+}
---
+++ C1_Fundamentos/readme.txt
@@ -0,0 +1,6 @@
+Para executar o programa, executar os seguintes comandos no cmd (Prompt de Comando), em instâncias separadas e simultâneas:
+Dentro da pasta api: npm start
+Dentro da pasta alurapic2: ng s -o
+
+Com isso, o site abrirá automaticamente em seu navegador. Você pode acessá-lo, também, através do endereço http://localhost:4200
+Para visualizar o que foi feito durante o curso, acesse o endereço http://localhost:4200/user/flavio
