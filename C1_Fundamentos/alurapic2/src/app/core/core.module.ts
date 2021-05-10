c061270fe15343c2f1542060cade4c4d3bb1c607
lucasamacedo <lucas_am_@hotmail.com>
2021-05-10 14:17:09 -0300

    Criando rodapé

--- C1_Fundamentos/alurapic2/src/app/core/core.module.ts
+++ C1_Fundamentos/alurapic2/src/app/core/core.module.ts
@@ -1,21 +1,24 @@
-import { RequestInterceptor } from './auth/request.interceptor';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { HeaderComponent } from './header/header.component';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FooterComponent } from './footer/footer.component';
+import { RequestInterceptor } from './auth/request.interceptor';
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
     RouterModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   providers: [
     {
