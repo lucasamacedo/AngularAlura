685f6769ec15c78ee72649258703c0b8552595e8
lucasamacedo <lucas_am_@hotmail.com>
2021-05-10 04:42:20 -0300

    Detectando plataforma sendo usada

--- C1_Fundamentos/alurapic2/src/app/home/signin/signin.component.ts
+++ C1_Fundamentos/alurapic2/src/app/home/signin/signin.component.ts
@@ -2,7 +2,8 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
-import { AuthService } from './../../core/auth.service';
+import { PlatformDetectorService } from './../../core/platform-detector/platform-detector.service';
+import { AuthService } from './../../core/auth/auth.service';
 
 @Component({
   templateUrl: './signin.component.html'
@@ -14,7 +15,8 @@ export class SignInComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private platformDetectorService: PlatformDetectorService
     ) {}
 
   ngOnInit(): void {
@@ -41,7 +43,9 @@ export class SignInComponent implements OnInit {
         err => {
           console.log(err);
           this.loginForm.reset();
-          this.userNameInput?.nativeElement.focus();
+          if(this.platformDetectorService.isPlatformBrowser()){
+            this.userNameInput?.nativeElement.focus();
+          }
           alert('Invalid user name or password');
         }
       );
