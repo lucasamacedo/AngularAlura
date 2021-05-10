857fd9d50f76be2ffe3358c834572aa809de3f61
lucasamacedo <lucas_am_@hotmail.com>
2021-05-10 10:18:46 -0300

    Começando a criar validação na página de registro de usuário

--- C1_Fundamentos/alurapic2/src/app/home/signup/signup.component.ts
+++ C1_Fundamentos/alurapic2/src/app/home/signup/signup.component.ts
@@ -1,8 +1,40 @@
-import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './signup.component.html'
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  signUpForm: FormGroup = new FormGroup({});
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.signUpForm = this.formBuilder.group({
+      email: ['',
+        [
+          Validators.required,
+          Validators.email
+        ]],
+      fullName: ['',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(40)
+        ]],
+      userName: ['',
+        [
+          Validators.required,
+          Validators.pattern(/^[a-z0-9_\-]+$/),
+          Validators.minLength(2),
+          Validators.maxLength(30)
+        ]],
+      password: ['',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(14)
+        ]]
+    });
+  }
 }
