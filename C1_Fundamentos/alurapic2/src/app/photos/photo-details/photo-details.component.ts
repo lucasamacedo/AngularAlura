5e9d4e03437d8d2a91f0f959041ebff78716c4b0
lucasamacedo <lucas_am_@hotmail.com>
2021-05-11 14:34:17 -0300

    Mostrando mensagem após remover foto

--- C1_Fundamentos/alurapic2/src/app/photos/photo-details/photo-details.component.ts
+++ C1_Fundamentos/alurapic2/src/app/photos/photo-details/photo-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
+import { AlertService } from './../../shared/components/alert/alert.service';
 import { Photo } from '../photo/photo';
 import { PhotoService } from './../photo/photo.service';
 
@@ -16,17 +17,26 @@ export class PhotoDetailsComponent implements OnInit{
   constructor(
     private route: ActivatedRoute,
     private photoService: PhotoService,
-    private router: Router) {}
+    private router: Router,
+    private alertService: AlertService) {}
 
   ngOnInit(): void {
     this.photoId = this.route.snapshot.params.photoId;
     this.photo$ = this.photoService.findById(this.photoId);
   }
 
-  remove() {
+  remove(): void {
     this.photoService
     .removePhoto(this.photoId)
-    .subscribe(() => this.router.navigate(['']));
+    .subscribe(
+      () => {
+      this.alertService.success('Photo removed');
+      this.router.navigate(['']);
+      },
+      err => {
+        console.log('erro');
+        this.alertService.warning('Could not delete photo!');
+      });
   }
 
 }
